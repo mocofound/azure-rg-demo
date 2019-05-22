30fdd050268f264cdfeebcb2e43e29959f52803c
Alex Harness <9669770+mocofound@users.noreply.github.com>
2019-05-22 12:15:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,14 +17,14 @@ resource "azurerm_virtual_network" "main" {
   location            = "${azurerm_resource_group.main.location}"
   address_space       = ["10.0.0.0/16"]
 }
-
+/*
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = "${azurerm_resource_group.main.name}"
   virtual_network_name = "${azurerm_virtual_network.main.name}"
   address_prefix       = "10.0.2.0/24"
 }
-/*
+
 resource "azurerm_network_interface" "main" {
   name                = "testvm1-nic"
   location            = "${azurerm_resource_group.main.location}"
